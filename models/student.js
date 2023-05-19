e5af223343e96a79a96452b780f3cd92d73772ba
Vaibhavihole31 <itspinki05@gmail.com>
2023-05-19 13:19:37 +0530

    feat: student model added

---
+++ models/student.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema({
+  fullName: { type: String, required: [true, 'fullName cannot be empty'] },
+  email: { type: String, required: [true, 'email cannot be empty'] },
+  mobile: { type: String, required: [true, 'mobile cannot be empty'] },
+},
+  {
+    timestamps: true
+  }
+)
+
+const Student = mongoose.model('Student', studentSchema);
+export default Student;
