87aea0daa6c969595d772ef81c1997f1b082c41f
Vaibhavihole31 <itspinki05@gmail.com>
2023-06-08 23:34:07 +0530

    feat: mongodb hooks added

--- server/models/student.js
+++ server/models/student.js
@@ -5,11 +5,23 @@ const studentSchema = new mongoose.Schema({
   fullName: { type: String, required: [true, 'fullName cannot be empty'] },
   email: { type: String, required: [true, 'email cannot be empty'] },
   mobile: { type: String, required: [true, 'mobile cannot be empty'] },
+  reg: {type: "String"}
 },
   {
     timestamps: true
   }
 )
 
+studentSchema.pre('save', async function(){
+  this.reg = Math.floor(Math.random() * 10000);
+})
+
+studentSchema.post('save', function(doc) {
+  console.log(`${doc.email} is saved`);
+});
+
 const Student = mongoose.model('Student', studentSchema);
+
+
+
 export default Student;
